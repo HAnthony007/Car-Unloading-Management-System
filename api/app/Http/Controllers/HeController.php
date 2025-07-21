b6ce10d8c79ad3e5ed2adb580098770d53ccc5a4
HAnthony007 <harifetraanthonyr@gmail.com>
2025-07-21 23:16:12 +0300

    feat: add he resource
    Adds model, migration, factory, seeder, policy,
    request and controller for he resource.
    Removes contributing section from README.

---
+++ api/app/Http/Controllers/HeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreheRequest;
+use App\Http\Requests\UpdateheRequest;
+use App\Models\he;
+
+class HeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreheRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(he $he)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(he $he)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateheRequest $request, he $he)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(he $he)
+    {
+        //
+    }
+}
